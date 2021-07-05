bb28f835300900f1c57b7933708fa32f3915c0c4
vemireddyarun0701 <vemireddyarun0701@gmail.com>
2021-07-05 21:49:48 +0530

    fc

--- src/main/java/com/javatechie/executor/api/controller/UserController.java
+++ src/main/java/com/javatechie/executor/api/controller/UserController.java
@@ -6,16 +6,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 
 @RestController
+@RequestMapping(path = "/api")
 public class UserController {
     @Autowired
     private UserService service;
@@ -41,5 +39,10 @@ public class UserController {
         CompletableFuture<List<User>> users3=service.findAllUsers();
         CompletableFuture.allOf(users1,users2,users3).join();
         return  ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    @GetMapping(path = "/welcome")
+    public ResponseEntity<String> welcomeApi(){
+        return ResponseEntity.ok("hi");
     }
 }
