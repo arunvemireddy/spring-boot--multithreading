d84b24dfa063f681f535393ffe5396c32d85a0a3
vemireddyarun0701 <vemireddyarun0701@gmail.com>
2021-07-05 21:51:40 +0530

    sc

--- src/main/java/com/javatechie/executor/api/controller/UserController.java
+++ src/main/java/com/javatechie/executor/api/controller/UserController.java
@@ -43,6 +43,6 @@ public class UserController {
 
     @GetMapping(path = "/welcome")
     public ResponseEntity<String> welcomeApi(){
-        return ResponseEntity.ok("hi");
+        return ResponseEntity.ok("hi,how are you");
     }
 }
