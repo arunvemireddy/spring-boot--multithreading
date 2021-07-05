b9fe8f7ce0e6f544f1f87c6ee99bc023cd7710b6
vemireddyarun0701 <vemireddyarun0701@gmail.com>
2021-07-05 22:02:14 +0530

    sc

--- src/main/java/com/javatechie/executor/api/controller/UserController.java
+++ src/main/java/com/javatechie/executor/api/controller/UserController.java
@@ -14,6 +14,7 @@ import java.util.concurrent.CompletableFuture;
 
 @RestController
 @RequestMapping(path = "/api")
+@CrossOrigin
 public class UserController {
     @Autowired
     private UserService service;
