2a47512fb85ab3bbc4c0fe3ae597f2f24bc6387c
vemireddyarun0701 <vemireddyarun0701@gmail.com>
2021-07-06 16:34:56 +0530

    foc

--- src/main/java/com/javatechie/executor/api/SpringbootMultithreadingExampleApplication.java
+++ src/main/java/com/javatechie/executor/api/SpringbootMultithreadingExampleApplication.java
@@ -2,11 +2,18 @@ package com.javatechie.executor.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 @EnableSwagger2
-public class SpringbootMultithreadingExampleApplication {
+public class SpringbootMultithreadingExampleApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootMultithreadingExampleApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootMultithreadingExampleApplication.class, args);
